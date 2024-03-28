8c5692b57c6b79888ecde7e9cc4f90dd00b2262b
Christian Rivera <chris.rivera9582@gmail.com>
2024-03-27 22:02:43 -0600

    remove tabview and add double button icon

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { View, Image, ActivityIndicator, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Image, Text, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { Platform } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
-import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
-
 
 export default function App() {
   const [image, setImage] = useState(null);
@@ -14,83 +12,51 @@ export default function App() {
   const [leaves, setLeaves] = useState(null);
   const [damagedLeaves, setDamagedLeaves] = useState(null);
   const [fruits, setFruits] = useState(null);
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    { key: 'camera', title: 'Camera' },
-    { key: 'gallery', title: 'Gallery' },
-  ]);
-
-  const CameraRoute = () => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      {image && <Image source={{ uri: image }} style={styles.mainImage} />}
-    </View>
-  );
-  
-  const GalleryRoute = () => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {image && <Image source={{ uri: image }} style={styles.mainImage} />}
-    </View>
-  );
 
   const pickImage = async () => {
     const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
     if (permissionResult.granted === false) {
       alert('Permission to access camera was denied.');
-    }else{
-      let result;
-      if (index === 0) {
-        result = await ImagePicker.launchCameraAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.Images,
-          base64: true,
-          quality: 1,
-        });
-      } else {
-        result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.Images,
-          base64: true,
-          quality: 1,
-        });
-      }
+    } else {
+      const result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        base64: true,
+        quality: 1,
+      });
+
       if (!result.cancelled && result.assets) {
         uploadToImgbb(result.assets[0].base64);
-      }else{
+      } else {
         setLoading(false);
       }
     }
   };
 
-// Upload image to imgbb
-const uploadToImgbb = async (base64Image) => {
-  try {
-    // Create a FormData object
-    const formData = new FormData();
-    formData.append('image', base64Image);
+  const uploadToImgbb = async (base64Image) => {
+    try {
+      const formData = new FormData();
+      formData.append('image', base64Image);
 
-    // Make the POST request
-    fetch('https://api.imgbb.com/1/upload?key=e39a11a3661e282d762593c2bcfee657', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      body: formData,
-    })
-      .then(response => response.json())
-      .then(data => {
-        setImage(data.data.url);
-        roboflowInference(data.data.url)
+      fetch('https://api.imgbb.com/1/upload?key=e39a11a3661e282d762593c2bcfee657', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        body: formData,
       })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+        .then(response => response.json())
+        .then(data => {
+          setImage(data.data.url);
+          roboflowInference(data.data.url);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    } catch (error) {
+      console.error('Error uploading image to Imgbb:', error);
+    }
+  };
 
-  } catch (error) {
-    console.error('Error uploading image to Imgbb:', error);
-    throw error;
-  }
-  
-};
-
-  //Make leaves, damaged leaves and fruit inferences
   const roboflowInference = async (ImgLink) => {
     try {
       const apiUrl = 'https://detect.roboflow.com/coffee-leaf-detection/4';
@@ -104,9 +70,6 @@ const uploadToImgbb = async (base64Image) => {
         api_key: apiKey,
         image: ImgLink
       };
-
-      const formData = new FormData();
-      formData.append('data', ImgLink);
 
       const requestConfig = {
         method: 'POST',
@@ -126,50 +89,43 @@ const uploadToImgbb = async (base64Image) => {
     }
   };
 
-  useEffect(()=> {
-    if(prediction?.predictions){
+  useEffect(() => {
+    if (prediction?.predictions) {
       const countInferences = async () => {
-          let leavesCountTemp = 0;
-          let damagedLeavesCountTemp = 0;
-          let fruitCountTemp = 0;
-  
-          prediction.predictions.forEach((prediction) => {
-            if (prediction.confidence > 0.65) {
-              switch (prediction.class) {
-                case 'h':
-                  leavesCountTemp += 1;
-                  break;
-                case 'hd':
-                  damagedLeavesCountTemp += 1;
-                  break;
-                case 'f':
-                  fruitCountTemp += 1;
-                  break;
-                default:
-                  break;
-              }
+        let leavesCountTemp = 0;
+        let damagedLeavesCountTemp = 0;
+
+        prediction.predictions.forEach((prediction) => {
+          if (prediction.confidence > 0.65) {
+            switch (prediction.class) {
+              case 'h':
+                leavesCountTemp += 1;
+                break;
+              case 'hd':
+                damagedLeavesCountTemp += 1;
+                break;
+              default:
+                break;
             }
-          });
-  
-          // Update state with counts
-          setLeaves(leavesCountTemp);
-          setDamagedLeaves(damagedLeavesCountTemp);
-          setFruits(fruitCountTemp);
+          }
+        });
+
+        setLeaves(leavesCountTemp);
+        setDamagedLeaves(damagedLeavesCountTemp);
       };
       countInferences();
-      
     }
     setLoading(false);
-  }, [prediction])
+  }, [prediction]);
 
-  useEffect(()=>{
-    if(prediction){
-      makePrediction(leaves, damagedLeaves)
+  useEffect(() => {
+    if (leaves !== null && damagedLeaves !== null) {
+      makePrediction(leaves, damagedLeaves);
     }
-  }, [fruits])
+  }, [leaves, damagedLeaves]);
 
-  const makePrediction = async (leaves, deadLeaves) => {
-    if(leaves){
+  const makePrediction = async (leaves, damagedLeaves) => {
+    if (leaves !== null) {
       fetch('http://kurisu95.pythonanywhere.com/predict', {
         method: 'POST',
         headers: {
@@ -177,31 +133,19 @@ const uploadToImgbb = async (base64Image) => {
         },
         body: JSON.stringify({
           leaves: leaves,
-          damaged_leaves: deadLeaves,
+          damaged_leaves: damagedLeaves,
         }),
       })
         .then(response => response.json())
         .then(data => {
-          setFruits(data.prediction)
+          const roundedNumber = Math.round(data.prediction);
+          setFruits(roundedNumber);
         })
         .catch(error => {
           console.error('Error:', error);
         });
-    }else{
-      setFruits(0)
-    }
-  };
-
-  const renderScene = SceneMap({
-    camera: CameraRoute,
-    gallery: GalleryRoute,
-  });
-
-  const renderCameraIcon = () => {
-    if (index === 0) {
-      return <MaterialIcons name="photo-camera" size={24} color="white" />;
     } else {
-      return <MaterialIcons name="image" size={24} color="white" />;
+      setFruits(0);
     }
   };
 
@@ -221,32 +165,20 @@ const uploadToImgbb = async (base64Image) => {
       <View style={styles.banner}>
         <Text style={styles.bannerText}>Coffee App Predictor</Text>
       </View>
-      <TabView
-        navigationState={{ index, routes }}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        initialLayout={{ width: '100%'}}
-        style={{ flex: 3 }}
-        renderTabBar={props => (
-          <TabBar
-            {...props}
-            indicatorStyle={{ backgroundColor: '#fff' }} // Color of the selected tab indicator
-            style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', elevation: 0 }} // Background color of the tab bar and shadow depth
-          />
-        )}
-      />
       <View style={styles.content}>
-        <TouchableOpacity style={styles.cameraButton} onPress={pickImage}>
-          {renderCameraIcon()}
-        </TouchableOpacity>
+        {image && <Image source={{ uri: image }} style={styles.mainImage} />}
+        <View style={styles.iconContainer}>
+          <TouchableOpacity style={styles.cameraButton} onPress={pickImage}>
+            <MaterialIcons name="photo-camera" size={24} color="white" />
+          </TouchableOpacity>
+        </View>
         {loading && <ActivityIndicator size="large" color="#0000ff" />}
-        {!loading && fruits !== undefined && leaves !== undefined && damagedLeaves !== undefined && prediction && (
+        {!loading && fruits !== null && leaves !== null && damagedLeaves !== null && prediction && (
           <View>
             <Text style={styles.predictionText}>Leaves: {leaves} - Damaged leaves: {damagedLeaves}</Text>
             <Text style={styles.predictionText}>Predicted fruit production: {fruits}</Text>
           </View>
         )}
-        
       </View>
     </View>
   );
@@ -270,7 +202,7 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
-    margin: 10
+    margin: 10,
   },
   bannerText: {
     color: 'white',
@@ -289,14 +221,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   mainImage: {
-    width: '95%',
-    height: '100%',
-    marginTop: 20,
+    width: '80%', 
+    height: '70%',
+    marginBottom: 20,
   },
   cameraButton: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     borderRadius: 50,
     padding: 15,
     margin: 10,
+  },
+  iconContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '50%',
   },
 });
